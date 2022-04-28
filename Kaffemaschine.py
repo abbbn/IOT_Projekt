8004ff4c8f1d49c8ebf2e636bc290707f3ff836f
fifthaccess <benedikt.erkner-sach@bulme.at>
2022-04-28 11:12:02 +0200

    add toJson

--- Kaffemaschine.py
+++ Kaffemaschine.py
@@ -1,11 +1,12 @@
-import logging
-from azure.data.tables import TableServiceClient
-from datetime import datetime
-import azure.functions as func
-import json 
-connection_string = "DefaultEndpointsProtocol=https;AccountName=uc3bhwii;AccountKey=XUEDCBkPBnQg1Hb2DR2lYDigRRzKcNLYO98AlbLTbLIgRr2ivt94gTxFgByvJwnJe1Ui/tifNCnxURTKBdJM8A==;EndpointSuffix=core.windows.net"
-service = TableServiceClient.from_connection_string(conn_str=connection_string)
+from time import sleep
+from typing_extensions import Self
+from azure.iot.device import IoTHubModuleClient
+from random import randint
+import datetime 
+import json
 
+RECEIVED_MESSAGES = 0 
+Kaffesorte = "KaffeLatte" 
 
 class Kaffee_maschine():
     def __init__(self) :
@@ -14,33 +15,28 @@ class Kaffee_maschine():
         self.Kaffee_menge = 0
         self.Kaffee_sorte = ""
         
-    def fromJson(messwert_als_json_string):
-        messwert_geparst = json.loads(messwert_als_json_string)
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, 
+            sort_keys=True, indent=4)
 
-        messwertObjekt = Kaffee_maschine()
-        messwertObjekt.Kaffee_Maschine_ID = messwert_geparst["Kaffee_Maschine_ID"]
-        messwertObjekt.Time_of_execution = messwert_geparst["Time_of_execution"]
-        messwertObjekt.Kaffee_menge = messwert_geparst["Kaffee_menge"]
-        messwertObjekt.Kaffee_sorte = messwert_geparst["Kaffee_sorte"]
-        #messwertObjekt.status = messwert_geparst["status"]
 
-        return messwertObjekt
+def main ():
+    connection_string = "HostName=iot3bhwii22-es.azure-devices.net;DeviceId=Notebook-es;SharedAccessKey=S+VsJvpF90xN6m9VLrTdb5/YurTU6DhrNUCRwHUYL3c="
+    client = IoTHubModuleClient.create_from_connection_string(connection_string) 
 
-def main(msg: func.ServiceBusMessage):
-    
-    PRODUCT_ID = u'001234'
-    PRODUCT_NAME = u'RedMarker'
+   # messages = [send_message ("RECEIVED_MESSAGES ++ Kaffesorte") for _ in range(5)]
 
-    my_entity = {
+    client.connect()
 
-        u'Kaffee_Maschine_ID': PRODUCT_ID,
-        u'Time_of_execution': PRODUCT_NAME,
-        u'Kaffee_menge': 15,
-        u'Kaffee_sorte': 9.99,
-        u'PurchaseDate': datetime(1973, 10, 4),
+    client.send_message(Kaffesorte)
+    client.disconnect()
+    client.shutdown()
 
-    }
+def massage_receiver ():
+    global RECEIVED_MESSAGES
+    RECEIVED_MESSAGES += 1
+    print("")
+    print("Message received:")
 
-    table_client = service.get_table_client(table_name="kaffeDatensatz")
-
-    entity = table_client.create_entity(entity=my_entity)
+main()
+#massage_receiver()
