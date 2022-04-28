f03b1b79f0f72122924d392b382ab09625221d4f
fifthaccess <benedikt.erkner-sach@bulme.at>
2022-04-28 10:43:54 +0200

    add json object

--- Kaffemaschine.py
+++ Kaffemaschine.py
@@ -2,17 +2,23 @@ from time import sleep
 from typing_extensions import Self
 from azure.iot.device import IoTHubModuleClient
 from random import randint
+import datetime 
+import json
 
 RECEIVED_MESSAGES = 0 
 Kaffesorte = "KaffeLatte" 
 
+class Kaffee_maschine():
+    def __init__(self) :
+        self.Kaffee_Maschine_ID = ""
+        self.Time_of_execution = datetime()
+        self.Kaffee_menge = 0
+        self.Kaffee_sorte = ""
+        
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, 
+            sort_keys=True, indent=4)
 
-def send_a_list_of_messages(sender):
-    # create a list of messages
-    messages = [ServiceBusMessage("Message in list") for _ in range(5)]
-    # send the list of messages to the queue
-    sender.send_messages(messages)
-    print("Sent a list of 5 messages")
 
 def main ():
     connection_string = "HostName=iot3bhwii22-es.azure-devices.net;DeviceId=Notebook-es;SharedAccessKey=S+VsJvpF90xN6m9VLrTdb5/YurTU6DhrNUCRwHUYL3c="
